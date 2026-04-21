63a4374342ec16b668830b7bd8a8d9c6216cb311
Mohammed <mohammedsuhailr2@gmail.com>
2026-04-21 18:19:10 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const add = () => {};
+
+const subtract = () => {};
+
+const multiply = () => {};
+
+const divide = () => {};
+
+const operate = (operater, num1, num2) => {};
+
+const clear = () => {};
